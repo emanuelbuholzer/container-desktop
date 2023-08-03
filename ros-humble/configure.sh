2ee044bd8ae46f81a3da6d039820bcb1185310fe
Emanuel Buholzer <emanuel0xb@gmail.com>
2023-08-03 10:10:41 +0200

    Create ros-humble image

---
+++ ros-humble/configure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Determine the script directory, in which the parent build.conf file is stored
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do
+  SCRIPT_DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE
+done
+SCRIPT_DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
+
+# Source the parent build configuration
+source "$SCRIPT_DIR"/../build.conf
